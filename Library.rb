c3f5cf4b3388ae374a27f740783561a9ef7c713b
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2019-09-11 23:47:14 +0900

    add

--- Library.rb
+++ Library.rb
@@ -7,3 +7,24 @@
 # 最大公約数と最小公倍数を同時に求めて配列で返す。
 624.gcdlcm(735)
 
+# 幅優先探索
+# 深さ優先探索
+
+# 入力が地図のパターン
+h, w = gets.split.map(&:to_i)
+c = h.times.map {gets.chomp} # 文字列の配列
+c = h.times.map {gets.chomp.chars}
+
+# 入力が二次元配列のパターン
+h, w = gets.split.map(&:to_i)
+a = h.times.map {gets.split.map(&:to_i)}
+
+# 入力が幅と高さのみ。二次元配列を自分で作る場合。
+h, w = gets.split.map(&:to_i)
+a = h.times.map{|i|}
+  w.times.map {|j|
+    # i,jによる初期値
+    # i == j ? 1 : 0 単位行列
+  }
+}
+
