5e01b417685dccceffdb943c85fc4797784b18ad
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2019-09-21 11:22:16 +0900

    fix

--- ABC141/B.rb
+++ ABC141/B.rb
@@ -1,29 +1,19 @@
-def odd_stepable?(strs)
-  strs.each do |str|
-    if str == 'R' || str == 'U' || str == 'D'
-      return true
-    else
-      return  false
-    end
-  end
-end
-
-def even_stepable?(strs)
-  strs.each do |str|
-    if str == 'L' || str == 'U' || str == 'D'
-      return true
-    else
-      return  false
-    end
-  end
-end
-
 str = gets.chomp.split('')
 
-odd_strs = str.group_by{|i| i%2}[1]
-even_strs = str.group_by{|i| i%2}[0]
-if odd_stepable?(odd_strs) && even_stepable?(even_strs)
-  puts 'Yes'
-else
-  puts 'No'
+(0..str.length-1).each do |i|
+  if i % 2 == 0
+    unless str[i] == 'R' || str[i] == 'U' || str[i] == 'D'
+      puts 'No'
+      return
+    end
+  end
+
+  if i % 2 == 1
+    unless str[i] == 'L' || str[i] == 'U' || str[i] == 'D'
+      puts 'No'
+      return
+    end
+  end
 end
+
+puts 'Yes'
