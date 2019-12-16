73ad2e95543e8ebc6d809cc7467c0f1308334d3a
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2019-12-16 20:46:20 +0900

    add

--- ABC/ABC132/A.rb
+++ ABC/ABC132/A.rb
@@ -1,15 +1,6 @@
-n, l = gets.split.map(&:to_i)
-
-tastes = []
-
-(1..n).each do |i|
-  tastes.push(l + i - 1)
-end
-
-sorted_tastes = tastes.sort
-
-if sorted_tastes[0] >= 0
-  puts (sorted_tastes[1..(sorted_tastes.length-1)]).inject(:+)
+s = gets.split("")
+if s.uniq.length == 3
+  print("Yes")
 else
-  puts (sorted_tastes[0..(sorted_tastes.length-2)]).inject(:+)
+  print("No")
 end
