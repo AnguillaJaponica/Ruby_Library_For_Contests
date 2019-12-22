9e97e723fb550c79112b902040344e9e797197c6
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2019-12-22 23:47:22 +0900

    add ABC085A, ABC148D

--- ABC/ABC148/D.rb
+++ ABC/ABC148/D.rb
@@ -1,19 +1,9 @@
 n = gets.to_i
-blocks = gets.split.map(&:to_i)
+a = 1
 
-point = 1
-flag = false
-
-blocks.each do |block|
-  if block == point
-    blocks.slice!(0..blocks.index(block))
-    point += 1
-    flag = true
-  end
+gets.split.each do |block|
+  block = block.to_i
+  a += 1 if a == block
 end
 
-if !flag
-  puts -1
-else
-  puts n - point
-end
+puts a == 1 ? -1 :  n - a + 1
