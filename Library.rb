bdadedfeddbbf3fa32e5216d7bf12ae2b65dd2df
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2019-12-31 16:14:44 +0900

    update lubrary

--- Library.rb
+++ Library.rb
@@ -100,3 +100,26 @@ p "処理概要 #{Time.now - start_time}s" # 最後に挿入
 
 # 階乗
 (1..n).inject(1,:*)
+
+# メモ化再帰
+def fib2(n)
+  array = []
+  return 1 if n <= 1
+  if array[n]
+    array[n]
+  else
+    array[n] = fib2(n - 1) + fib2(n - 2)
+  end
+end
+
+# 分割統治法/漸化式ループ
+def fib3(n)
+  return 1 if n == 0
+  fib1 = fib2 = fib3 = 1
+  (n-1).times do
+    fib3 = fib1 + fib2
+    fib1 = fib2
+    fib2 = fib3
+  end
+  fib2
+end
