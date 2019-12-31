22709f8586dc9917386c92db14647bef0a746084
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2019-12-31 16:50:24 +0900

    ABC128b

--- Library.rb
+++ Library.rb
@@ -1,31 +1,33 @@
-# 入出力
- = gets.split.map(&:to_i)
+# メモ化再帰
+def fib2(n)
+  array = []
+  return 1 if n <= 1
+  if array[n]
+    array[n]
+  else
+    array[n] = fib2(n - 1) + fib2(n - 2)
+  end
+end
 
+# 分割統治法/漸化式ループ
+def fib3(n)
+  return 1 if n == 0
+  fib1 = fib2 = fib3 = 1
+  (n - 1).times do
+    fib3 = fib1 + fib2
+    fib1 = fib2
+    fib2 = fib3
+  end
+  fib2
+end
 
-# 文字列を一文字ずつの文字の配列にする
-str.chars
+# ハッシュソート
+scores = {'Carol' => 90, 'Alice' => 50, 'Bob' => 60, 'David' => 40}
+p scores.sort {|(k1, v1), (k2, v2)| v2 <=> v1}
 
-# ２進数にする
-255.to_s(2) # 11111111
-
-# 最小公倍数
-625.lcm(735)
-
-# 最大公約数
-625.gcd(735)
-
-# 最大公約数と最小公倍数を同時に求めて配列で返す。
-624.gcdlcm(735)
-
-# 1文字ずつ分解
-str.split('')
-
-# 配列に含まれる要素の種類の数
-# 文字列にuniqは使えない
-array.uniq.length
 
 # 3桁表示で、ない分は0を埋める
-sprintf"%03d", i
+sprintf "%03d", i
 
 # 素数判定
 def check_prime(num)
@@ -35,7 +37,7 @@ def check_prime(num)
   when 2 then
     true
   else
-    (2..(num-1)).each do |index|
+    (2..(num - 1)).each do |index|
       if num % index == 0
         return false
       end
@@ -44,18 +46,9 @@ def check_prime(num)
   end
 end
 
-# 文字列一つ一つのループ
-str.each_char
-
 # 幅優先探索
 # 深さ優先探索
 # 入力が地図のパターン
-#
-#
-h, w = gets.split.map(&:to_i)
-c = h.times.map {gets.chomp} # 文字列の配列
-c = h.times.map {gets.chomp.chars} # 一文字ずつ切る
-c = gets.split(' ').map(&:to_i) # 数値の場合
 
 # 戦闘だけ大文字
 str.capitalize
@@ -71,7 +64,7 @@ a = h.times.map {gets.split.map(&:to_i)}
 
 # 入力が幅と高さのみ。二次元配列を自分で作る場合。
 h, w = gets.split.map(&:to_i)
-a = h.times.map{|i|
+a = h.times.map {|i|
   w.times.map {|j|
     # i,jによる初期値
     # i == j ? 1 : 0 単位行列
@@ -79,8 +72,8 @@ a = h.times.map{|i|
 }
 
 # 配列の奇数番目や偶数番目を取り出して新たな配列にする
-odd = ary.group_by{|i| i%2}[1]
-even = ary.group_by{|i| i%2}[0]
+odd = ary.group_by {|i| i % 2}[1]
+even = ary.group_by {|i| i % 2}[0]
 
 # 数値からなる配列のうち、奇数のものだけ・偶数のものだけとる
 odd = ary.select(&:odd?)
@@ -99,27 +92,4 @@ start_time = Time.now
 p "処理概要 #{Time.now - start_time}s" # 最後に挿入
 
 # 階乗
-(1..n).inject(1,:*)
-
-# メモ化再帰
-def fib2(n)
-  array = []
-  return 1 if n <= 1
-  if array[n]
-    array[n]
-  else
-    array[n] = fib2(n - 1) + fib2(n - 2)
-  end
-end
-
-# 分割統治法/漸化式ループ
-def fib3(n)
-  return 1 if n == 0
-  fib1 = fib2 = fib3 = 1
-  (n-1).times do
-    fib3 = fib1 + fib2
-    fib1 = fib2
-    fib2 = fib3
-  end
-  fib2
-end
+(1..n).inject(1, :*)
