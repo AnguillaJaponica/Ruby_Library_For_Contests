da0a65d4c28db04580da4c89f5165e9142844d68
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2019-12-31 15:39:03 +0900

    fixABC006B

--- ABC/ABC006/B.rb
+++ ABC/ABC006/B.rb
@@ -4,10 +4,10 @@ def tribonacci(n)
   num1 = 0
   num2 = 0
   num3 = 1
-  tmp = 0
+  tmp = 1
 
-  n.times do
-    tmp = num1 + num2 + num3
+  (n-3).times do
+    tmp = (num1 + num2 + num3) % 10007
     num1 = num2
     num2 = num3
     num3 = tmp
@@ -16,4 +16,4 @@ def tribonacci(n)
 end
 
 n = gets.to_i
-puts tribonacci(n-3) % 10007
+puts tribonacci(n)
