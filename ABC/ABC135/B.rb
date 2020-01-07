2ee0521545c445ab42a21dc8594263a90758045f
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2020-01-07 23:00:11 +0900

    ac ABC135B

--- ABC/ABC135/B.rb
+++ ABC/ABC135/B.rb
@@ -1,21 +1,17 @@
 n = gets.to_i
 array = gets.split.map(&:to_i)
-sorted_array = array.sort
+one_to_n = (1..n).to_a
+count = 0
+ans = 'NO'
 
-flag = false
-
-array.combination(2) do |i, j|
-  temp = i
-  i = j
-  j = temp
-
-  if array == sorted_array
-    flag = true
+(0..n-1).each do |i|
+  if array[i] != one_to_n[i]
+    count += 1
   end
 end
 
-if flag
-  puts 'YES'
-else
-  puts 'NO'
+if count <= 2
+  ans = 'YES'
 end
+
+puts ans
