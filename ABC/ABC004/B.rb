189a5d813a6a4df0958f5b225dddbcb78892c762
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2020-01-10 22:33:40 +0900

    ABC004B

---
+++ ABC/ABC004/B.rb
@@ -0,0 +1,9 @@
+a = gets.chomp.split(" ")
+b = gets.chomp.split(" ")
+c = gets.chomp.split(" ")
+d = gets.chomp.split(" ")
+
+puts d.reverse.join(" ")
+puts c.reverse.join(" ")
+puts b.reverse.join(" ")
+puts a.reverse.join(" ")
