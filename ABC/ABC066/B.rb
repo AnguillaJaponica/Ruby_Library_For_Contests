101707acc2d97bdcef0dc57e3928d58c2f4ad05d
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2020-01-11 20:34:44 +0900

    abc66B

--- ABC/ABC066/B.rb
+++ ABC/ABC066/B.rb
@@ -1,5 +1,12 @@
 s = gets.chomp
 
-(1..s.length/2).each do |i|
-  if
+def even_str?(str)
+  str[0..str.length/2-1] == str[str.length/2..-1]
+end
+
+(1..(s.length-2)/2).each do |i|
+  if even_str?(s[0..-2*i-1])
+    puts s[0..-2*i-1].length
+    break
+  end
 end
