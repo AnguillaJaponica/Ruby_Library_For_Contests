fbd307aab6c3f1e54b3b6190f6d3a6d3beba2831
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2020-01-12 11:16:48 +0900

    fix library

--- Library.rb
+++ Library.rb
@@ -144,3 +144,10 @@ end
     end
   end
 end
+
+# 回転
+mat = [[1,2,3],[4,5,6],[7,8,9]]
+
+pp mat.transpose.map(&:reverse)  #=> 右回転
+pp mat.reverse.map(&:reverse)    #=> 180度回転
+pp mat.transpose.reverse         #=> 左回転
