5228640a7e99ff4fd8cedb24cd1d891a85794ef8
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2020-01-16 22:04:47 +0900

    abc116B

--- ABC/ABC116/B.rb
+++ ABC/ABC116/B.rb
@@ -1,23 +1,19 @@
 s = gets.to_i
-collatz = []
-count = 1
 
-(1..1000000).each do |i|
-  if i.even?
-    if collatz.include?(i / 2)
-      break
-    else
-      collatz << i / 2
-      count += 1
-    end
+list = []
+list[s] = 1
+
+(2..1000000).each do |i|
+  if s.even?
+    s /= 2
   else
-    if collatz.include?(3 * i + 1)
-      break
-    else
-      collatz << 3 * i + 1
-      count += 1
-    end
+    s = 3 * s + 1
+  end
+
+  if list[s]
+    puts i
+    break
+  else
+    list[s] = 1
   end
 end
-
-puts count
