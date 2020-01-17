17a5d449f302bd42e807a2d52d33ccb3a12b15cb
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2020-01-17 22:42:55 +0900

    add

--- ABC/ABC100/B.rb
+++ ABC/ABC100/B.rb
@@ -1,9 +1,2 @@
-d, n = gets.split.map(&:to_i)
-
-if d == 0
-  puts n
-elsif d == 1
-  puts n * 100
-else
-  puts n * 10000
-end
+d,n = gets.split.map(&:to_i)
+puts n == 100 ? 100 ** d * 101 : 100 ** d * n
--- ABC/ABC105/B.rb
+++ ABC/ABC105/B.rb
@@ -1,2 +1,18 @@
 n = gets.to_i
+ans = 'No'
 
+(0..25).each do |i|
+  (0..14).each do |j|
+    sum = 4 * i + 7 * j
+    if sum > n || sum < n
+      next
+    else
+      if sum == n
+        ans = 'Yes'
+        break
+      end
+    end
+  end
+end
+
+puts ans
