aba5950fb7a987064d689f4083a4cb09b877c3e6
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2020-04-25 14:09:49 +0900

    ABC135C

--- ABC/ABC135/C.rb
+++ ABC/ABC135/C.rb
@@ -1,18 +1,13 @@
 n = gets.to_i
-monsters = gets.split.map(&:to_i).sort.reverse
-warriors = gets.split.map(&:to_i).reverse
+monsters = gets.split.map(&:to_i)
+warriors = gets.split.map(&:to_i)
 count = 0
 
-(0..n - 1).each do |i|
-  rest = warriors[i] - monsters[i]
-  if rest > 0
-    count += warriors[i]
-    monsters[i] -= warriors[i]
-    monsters[i + 1] -= rest
-  else
-    warriors
-    monsters[i] -= warriors[i]
-  end
+(0..n-1).each do |i|
+  count += [monsters[i], warriors[i]].min
+  warriors[i] = [warriors[i] - monsters[i], 0].max
+  count += [monsters[i+1], warriors[i]].min
+  monsters[i+1] = [monsters[i+1] - warriors[i], 0].max
 end
 
 puts count
